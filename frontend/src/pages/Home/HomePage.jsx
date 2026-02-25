e83771c96ae628d181bdbb83061480844ae72d5f
Nathan Gallagher <nathangallagher101@gmail.com>
2026-02-25 10:35:32 +0000

    landing page design

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
+import "../../App.css";
 
 export function HomePage() {
   return (
-    <div className="container text-center mt-5">
-      <h1>Welcome to AfterCredits!</h1>
+    <div className="splashscreen text-center">
+      <h1 className="mb-2 text-center text-warning Title">AfterCredits</h1>
+      <p role='paragraph' className="mb-4 text-center">Discover and Celebrate the Films you Love</p>
       <div className="d-flex justify-content-center gap-3 mt-3">
-        <Link to="/signup" className="btn btn-primary">Sign Up</Link>
-        <Link to="/login" className="btn btn-outline-primary">Log In</Link>
+        <Link to="/signup" className="btn btn-warning">Sign Up</Link>
+        <Link to="/login" className="btn btn-outline-warning">Log In</Link>
       </div>
     </div>
   );
