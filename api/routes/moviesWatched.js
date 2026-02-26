d6ec502b2e0b61ba0ca60835f1c2eba810aa5b3b
williamharrison <will.harri12@gmail.com>
2026-02-26 12:36:20 +0000

    add routes and functions for deletion for watch list and watched toggle

--- api/routes/moviesWatched.js
+++ api/routes/moviesWatched.js
@@ -6,5 +6,6 @@ const moviesWatchedController = require('../controllers/moviesWatched')
 router.get("/", moviesWatchedController.getAllPosts);
 router.post("/", moviesWatchedController.createPost);
 router.post("/:movieId", moviesWatchedController.markAsWatched);
+router.delete("/:movieId", moviesWatchedController.removeFromWatched);
 
 module.exports = router;
