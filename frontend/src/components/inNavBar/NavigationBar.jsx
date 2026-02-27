d823a346082d2fa08370601c470ffa0f62c31335
Nathan Gallagher <nathangallagher101@gmail.com>
2026-02-27 12:14:24 +0000

    got rid of home from navbar and font change for logo

--- frontend/src/components/inNavBar/NavigationBar.jsx
+++ frontend/src/components/inNavBar/NavigationBar.jsx
@@ -20,7 +20,6 @@ const NavigationBar = ({loggedIn,setLoggedIn}) => {
         {/* Right side buttons */}
         <div className="d-flex align-items-center gap-3">
           {loggedIn && <Link className="nav-link" to="/users/all">All Users</Link>}
-          {loggedIn && <Link className="nav-link" to="/movies">Home Page</Link>}
           {loggedIn && <Link className="nav-link" to="/browse">Browse</Link>}
           {loggedIn && <Link className="nav-link" to="/users/myprofile">Your Profile</Link>}
           {loggedIn && <Link className="nav-link" to="/account">Account Settings</Link>}
