2afebd9137c9b918b0b45a050f2ddc1f7cec1e66
williamharrison <will.harri12@gmail.com>
2026-03-03 13:58:05 +0000

    made profile and all users pages a it more readable and stylised

--- frontend/src/pages/Users/AllUsersPage.jsx
+++ frontend/src/pages/Users/AllUsersPage.jsx
@@ -22,18 +22,40 @@ const AllUsersPage = () => {
         })
     },[navigate])
 
-    return (
-    <>
-        <h2>All Users</h2>
-        <p>List of all the users on the platform</p>
-        <ul>
-        {users.map((user) => (
-        <User key={user._id} user={user}/>
-        ))}
-        </ul>
-    </>
-    );
+return (
+  <div className="container mt-4">
 
+    {/* Page Header */}
+    <div className="text-center mb-4">
+      <h2 className="fw-bold">Other Users</h2>
+      <p className="text-muted">
+        Discover other members of the Community
+      </p>
+    </div>
+
+    {users.length === 0 ? (
+      <div className="card p-4 text-center shadow-sm">
+        <p className="text-muted mb-0">
+          No users found.
+        </p>
+      </div>
+    ) : (
+      <div className="row">
+        {users.map((user) => (
+          <div
+            key={user._id}
+            className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4"
+          >
+            <div className="card h-100 shadow-sm text-center p-3">
+              <User user={user} />
+            </div>
+          </div>
+        ))}
+      </div>
+    )}
+
+  </div>
+);
 }
 
 export default AllUsersPage
