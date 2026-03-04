15413d3c142ca2a58fe9776dab76153763657d7f
Nathan Gallagher <nathangallagher101@gmail.com>
2026-03-04 15:05:26 +0000

    search bar on all users page

--- frontend/src/pages/Users/AllUsersPage.jsx
+++ frontend/src/pages/Users/AllUsersPage.jsx
@@ -7,6 +7,7 @@ const AllUsersPage = () => {
 
     const navigate = useNavigate();
     const [users,setUsers] = useState([]);
+    const [search, setSearch] = useState("");
 
     useEffect(()=> {
         const token = localStorage.getItem("token")
@@ -22,6 +23,12 @@ const AllUsersPage = () => {
         })
     },[navigate])
 
+const filteredUsers = users.filter(
+  (user) => {
+      return user.username.toLowerCase().includes(search.toLowerCase())
+  }
+)
+
 return (
   <div className="container mt-4">
     <div className="text-center mb-4">
@@ -30,8 +37,19 @@ return (
         Discover other members of the Community
       </p>
     </div>
+    <div className="input-group mb-4">
+      <span className="input-group-text">Search</span>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="form-control"
+        aria-label="Search"
+        placeholder="Search For Users"
+        />
+    </div>
 
-    {users.length === 0 ? (
+    {filteredUsers.length === 0 ? (
       <div className="card p-4 text-center shadow-sm">
         <p className="text-muted mb-0">
           No users found.
@@ -39,7 +57,7 @@ return (
       </div>
     ) : (
       <div className="row">
-        {users.map((user) => (
+        {filteredUsers.map((user) => (
           <div
             key={user._id}
             className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4"
